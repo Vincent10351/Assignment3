4d145c03abf860da130fbd80bca15bc09d29e191
Vincent Le Nguyen <vinceln3@circinus-48.ics.uci.edu>
2023-02-15 17:25:15 -0800

    added DEV test files

--- Main.py
+++ Main.py
@@ -4,12 +4,27 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 from nltk.stem import PorterStemmer
 import os
+import json
+
 
 index_file = 'index_file'
+document_count = 0
+
 
 def parse_files(root):
-    for filename in os.listdir(root):
-        for json_file in os.listdir(filename):
-            pass
+    global document_count
+    for filename in os.listdir(root):                                               #opens the root directory
+        for json_files in os.listdir(os.path.join(root, filename)):                 #opens each file within the root directory
+            with open(os.path.join(root, filename, json_files)) as json_file:       #opens each json_file within the sub-directory
+                loaded_json = json.load(json_file)                                  #loads each json_file 
+                content = loaded_json['content']
+                text = BeautifulSoup(content, 'lxml')
+                print (text)
+                document_count += 1
+    print (document_count)
 
 parse_files('ANALYST')
+
+#Wednesday: All day 
+#Thursday: 6pm - whenever
+#Friday: All day 
