6fc7f1562ab1fc4f408049da8aae719f3ab3ed72
Chia Chi Sun <chiacs@circinus-28.ics.uci.edu>
2023-03-09 20:54:35 -0800

    index fix push

--- Index.py
+++ Index.py
@@ -10,6 +10,7 @@ from nltk.tokenize import RegexpTokenizer
 import os, re, json
 import tokenizer
 from flask import Flask, render_template, url_for, request
+import time
 
 
 app = Flask(__name__)
@@ -32,13 +33,11 @@ Structure of the Inverted Index
                 id: int,                                 # doc ID number
                 term_frequency_in_doc: int,              # how many times the token appears in the document divided by total words in doc
                 tf_idf: int                              # tf * idf, i don't know how to explain
-                weight: int                            # weight of this token in this document
             }
             id2: {
                 id: int,
                 term_frequency_in_doc: int,
                 tf_idf: int
-                weight: int
             }
     }
 }
@@ -70,60 +69,81 @@ doc_id_dict = {}                                                #holds mapping o
 def ind(): return defaultdict(ind)
 index = ind()                                                   # a dictionary of all tokens
 
-def add_tokens(dict_tokens, doc_id): # Modified add_tokens to create .json file for each alphanumeric character. 
+def add_tokens(dict_tokens, doc_id):
+    # Calculate the total number of words in the document
     total_words = sum(dict_tokens.values())
+    
+    # Create an empty dictionary to store file data
+    file_data = {}
+    
+    # Loop through each token and its frequency in the document
     for token, frequency in dict_tokens.items():
-        token_letter = token[0].lower() # First letter of token lowercase
-        reg = re.compile(r'[a-z0-9]')   
-        if reg.match(token_letter):     # Checks if first letter matches a-z 0-9
-            if not token.isnumeric():   # Checks if the token is just a number ex. "1" , "100" , "1000", if so does not add to token file.
-                # Check if a file already exists for this token's first letter
-                filename = f'storage/partial/{token_letter}.json'
+        # Get the first letter of the token (converted to lowercase)
+        token_letter = token[0].lower()
+        
+        # Check if the first letter is an alphabetic character and the token is not just a number
+        if token_letter.isalpha() and not token.isnumeric():
+            # Create a filename for the token based on its first letter
+            filename = f'storage/partial/{token_letter}.json'
+            
+            # Check if the file data has already been loaded for this filename
+            if filename not in file_data:
+                # If the file doesn't exist, create it with an empty dictionary
                 if not os.path.exists(filename):
                     with open(filename, 'w') as f:
                         json.dump({}, f)
                 
-                # Open the file for this token's first letter
+                # Load the existing file data into the file_data dictionary
                 with open(filename, 'r+') as f:
-                    data = json.load(f)
+                    file_data[filename] = json.load(f)
+            
+            # Get the data dictionary for this token from the file data
+            data = file_data[filename]
+            
+            # If the token is not in the data dictionary, add it with initial values
+            if token not in data:
+                data[token] = {'token_frequency': 0, 'document_frequency': 0, 'doc_ids': {}}
+            
+            # Increment the document frequency and token frequency for this token
+            data[token]['document_frequency'] += 1
+            data[token]['token_frequency'] += frequency
+            
+            # Add the document ID, term frequency, and TF-IDF values for this token in this document
+            data[token]['doc_ids'][doc_id] = {'id': doc_id, 'term_frequency_in_doc': frequency / total_words, 'tf_idf': 0.0}
+    
+    # Write the updated file data back to each file
+    for filename, data in file_data.items():
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
                     
-                    # If the token is not in the file, add it
-                    if token not in data:
-                        data[token] = {'token_frequency': 0, 'document_frequency': 0, 'doc_ids': {}}
-                    
-                    # Update the token's information in the file
-                    data[token]['document_frequency'] += 1
-                    data[token]['token_frequency'] += frequency
-                    data[token]['doc_ids'][doc_id] = {'id': doc_id, 'term_frequency_in_doc': frequency / total_words, 'tf_idf': 0.0}
-                    
-                    # Write the updated data back to the file
-                    f.seek(0)
-                    json.dump(data, f, indent = 2)
-                    f.truncate()
-                    
+
 def nltk_tokenize(text : str):                                  #tokenizes the file and returns a list of tokens
     tokenizer = RegexpTokenizer(r'\w+')
     list_tokens = tokenizer.tokenize(text)                      
     return [stemmer.stem(token.strip()) for token in list_tokens]
 
 def calculate_tf_idf_score():                                   #calculates the tf_idf score for each document for each token
+    global index
     for token in index:
         for doc_id in index[token]['doc_ids']:
             idf = math.log(document_count / (1 + index[token]['doc_ids'][doc_id]['term_frequency_in_doc']), 10)
             tf = index[token]['doc_ids'][doc_id]['term_frequency_in_doc']
             index[token]['doc_ids'][doc_id]['tf_idf'] = tf * idf
+    with open('storage/fullIndex/merged_data.json','w') as f:
+        json.dump(index,f,indent = 4)
 
-def calculate_importance(soup, doc_id):
-    for header, base_weight in importance_dict.items():
-        for tag in soup.find_all(header):   # for each tag that matches header 
-            content = tag.text              # content of the tag
+# def calculate_importance(soup, doc_id):
+#     for header, base_weight in importance_dict.items():
+#         for tag in soup.find_all(header):   # for each tag that matches header 
+#             content = tag.text              # content of the tag
             
-            #TODO : tokenize contents of tag
-            tokens_in_tag = nltk_tokenize(content)
+#             #TODO : tokenize contents of tag
+#             tokens_in_tag = nltk_tokenize(content)
             
-            # Adds to weight of document according to header
-            for tag_token in tokens_in_tag: # iterate through token list
-                index[tag_token]['doc_ids'][doc_id]['term_frequency_in_doc'] += base_weight   # add weight 
+#             # Adds to weight of document according to header
+#             for tag_token in tokens_in_tag: # iterate through token list
+#                 if tag_token in index and doc_id in index[tag_token]['doc_ids']:
+#                     index[tag_token]['doc_ids'][doc_id]['term_frequency_in_doc'] += base_weight   # add weight 
         
 def parse_files(root):
     global document_count
@@ -139,12 +159,12 @@ def parse_files(root):
                 soup = BeautifulSoup(content, 'html.parser')
 
                 cur_list_tokens = nltk_tokenize(soup.get_text())
-                if len(cur_list_tokens) > 5000 or len(cur_list_tokens) < 200:           #prune high information or low information pages.
+                if len(cur_list_tokens) > 3000 or len(cur_list_tokens) < 200:           #prune high information or low information pages. Moz 2015 concluded avg webpage has 1891 tokens.
                     continue
                 if cur_list_tokens:                                                     #computes word frequencies and adds to index
                     word_frequencies = tokenizer.computeWordFrequencies(cur_list_tokens)
                     add_tokens(word_frequencies, document_count)
-                    calculate_importance(soup, document_count)                          #
+                    #calculate_importance(soup, document_count)                          
                 
                 doc_id_dict[document_count] = loaded_json['url']                        #stores mapping of docID to url
                 document_count += 1
@@ -172,20 +192,13 @@ def mergeIndices():
         merged_data.update(partial_index_data)
     with open('storage/fullIndex/merged_data.json','w') as f:
         json.dump(merged_data,f)
-    return
 
 
 def search(query):
     # Tokenize the query
     query_tokens = nltk_tokenize(query.lower())
     # Calculate the tf-idf score for each query token
-    query_tf_idf = {}
         
-    for token in query_tokens:
-        if token in index:
-            idf = math.log(document_count / index[token]['document_frequency'], 10)
-            tf = 1 + math.log(query_tokens.count(token), 10)
-            query_tf_idf[token] = tf * idf
 
     # Calculate the cosine similarity between the query and each document in the posting lists
     scores = {}
@@ -195,11 +208,12 @@ def search(query):
             for doc_id, posting in index[token]['doc_ids'].items():
                 if doc_id not in scores:
                     scores[doc_id] = 0.0
-                scores[doc_id] += query_tf_idf[token] * posting['tf_idf']
+                scores[doc_id] += index[token]['doc_ids'][doc_id]['tf_idf'] * posting['tf_idf']
                 if doc_id not in query_token_count:
                     query_token_count[doc_id] = 0
                 query_token_count[doc_id] += 1
-
+            
+        
     # Return the top 5 documents with the highest cosine similarity scores that contain all query tokens
     top_docs = [(doc_id, score) for doc_id, score in scores.items() if query_token_count[doc_id] == len(query_tokens)]
     top_docs = sorted(top_docs, key=lambda x: x[1], reverse=True)[:5]
@@ -216,6 +230,7 @@ def search(query):
 
 
 
+
 def start():
       
     if not os.path.exists('storage'):
@@ -223,6 +238,7 @@ def start():
     if not os.path.exists('storage/partial'):
         os.mkdir('storage/partial')
     parse_files('DEV')
+    load_dict()
     calculate_tf_idf_score()
                                                                          #calculate the tf_idf score of ALL tokens in index
     with open("storage/docID_mappings.json", "w+") as output_file:       #writes docID mappings to a file
@@ -231,14 +247,31 @@ def start():
     with open('storage/index_mappings.json', 'w+') as output_file:       #writes the index to a json file
         json.dump(index, output_file, indent = 4)
     
-
+    load_dict()
+    
     #search('cristina lopes')                              #performs the query on these terms
     #search('machine learning')
     #search('ACM')
     #search('master of software engineering')
-
+    #search('artificial')
+    #search('connect')
+    #search('algorithm')
+    #search('keong')  
+    #search('koagiri')
+    #search('magnetic field')
+    #search('XML')
+    #search('VR')
+    #search('Virtual RealiTy')
+    #search('UTC')
+    #search('ProfeSSor')
+    #search('Professor cristina lopes')
+    #search('zebra')
+    #search('ICS')
+    
 
 
 if __name__=='__main__':
+    start_time = time.time()
     start()
+    print("Indexing 20 Documents took: ",time.time()-start_time)
     #app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Assignment3
+# Assignment3 Search Engine
+Vincent Nguyen & Justin Chan & Jay Makwana & Ashley Sun
+
+Our search engine gathers data from approximately 56,000 pages from 88 different domains and organizes the data into an inverted index. In order to return query searches within 300ms and minimize memory , we implemented a partial index with separate files for each letter of the alphabet
+
+To use the search engine, we implemented a local web GUI with Flask. The user types in a query into the "Vincenettes Search Engine" and clicks the submit box. Afterwards, the user will receive the top K (5 in this case) links in an unordered list. The user can click on the hyperlink in order to directly go to the webpage. 
+(talk about flask/ui implementation)
